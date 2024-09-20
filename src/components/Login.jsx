f8b58779dc60d76314868c5c541932963b7ed66d
elyesszobli <elyesszobli@gmail.com>
2024-09-20 13:50:49 +0200

    creation du page de login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,14 @@
 import styles from './Login.module.scss'
 function Login(){
+
+    function handleClick(){
+        alert("click");
+    }
+
     return(
         <div className={styles.Container}>
-            Login
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Full_Logo_RGB_Black.png" alt="Spotify" />
+            <button onClick={handleClick}>Connect Spotify</button>
         </div>
     );
 }
