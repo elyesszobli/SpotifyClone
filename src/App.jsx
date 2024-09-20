f0646d3563b2275759e884760ce896227e4a176f
elyesszobli <elyesszobli@gmail.com>
2024-09-20 13:18:02 +0200

    mise en place du projet

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.module.scss'
+import Login from './components/Login'
+
+function App() {
+  
+  return (
+    <>
+      <Login/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import styles from './Login.module.scss'
+function Login(){
+    return(
+        <div className={styles.Container}>
+            Login
+        </div>
+    );
+}
+
+export default Login;
